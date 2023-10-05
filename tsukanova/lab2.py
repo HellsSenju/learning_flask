be3a480cafe205518b3495abfeae9db392dfaa71
Hells Hound <irisha1234517@gmail.com>
2023-10-05 07:55:26 +0400

    aaa

--- tsukanova/lab2.py
+++ tsukanova/lab2.py
@@ -2,14 +2,17 @@ from flask import Flask, request, render_template
 import pandas as pd
 import numpy as np
 
-#app = Flask(__name__)
+app = Flask(__name__)
 data = pd.read_csv('jio_mart_items.csv')
 data.dropna(inplace=True)
 
 # print(data[ data['category'] == 'Groceries']['items'].head())
 
-max = data.groupby(['category']).agg({'price' : 'max'})
 
+max = data.agg({'price' : 'max'})
+# max = data.groupby(['category']).agg({'price' : 'max'})
+# max.rename(columns={'price' : 'max'}, inplace = True)
+print(max)
 print(max.axes[0])
 print(max.axes[0].values) # закинуть в шаблон
 
@@ -28,9 +31,9 @@ task1 = 'Минимальная, максимальная, средняя цен
 task2 = 'Минимальная, максимальная, средняя цена в разрезе категорий'
 task3 = 'Минимальная, максимальная, средняя цена за килограмм'
 
-# @app.route('/')
-# def home():
-#     return max.to_html()
+@app.route('/')
+def home():
+    return max.to_html()
 
-# if __name__ == '__main__':
-#     app.run(debug=True, threaded=True)
+if __name__ == '__main__':
+    app.run(debug=True, threaded=True)
