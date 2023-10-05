b15b5814255660d757c74174d8918a17e12b19af
Inohara <InoSenju@Senju.com>
2023-10-05 11:15:31 +0400

    пупупууп

--- tsukanova/lab2.py
+++ tsukanova/lab2.py
@@ -3,37 +3,30 @@ import pandas as pd
 import numpy as np
 
 app = Flask(__name__)
+
 data = pd.read_csv('jio_mart_items.csv')
 data.dropna(inplace=True)
 
 # print(data[ data['category'] == 'Groceries']['items'].head())
 
 
-max = data.agg({'price' : 'max'})
-# max = data.groupby(['category']).agg({'price' : 'max'})
+category = data.groupby(['category']).agg({'price': ['min', 'max', 'mean']})
+sub_category = data.groupby(['sub_category']).agg({'price': ['min', 'max', 'mean']})
 # max.rename(columns={'price' : 'max'}, inplace = True)
-print(max)
-print(max.axes[0])
-print(max.axes[0].values) # закинуть в шаблон
-
-
-
-# df = pd.DataFrame([[1, 2, 3],
-#                    [4, 5, 6],
-#                    [7, 8, 9]],
-#                   columns=['A', 'B', 'C'])
-
-# print(df.agg(['min']))
+print(category)
+print(sub_category)
+# print(category.axes[0])
+# print(category.axes[0].values) # закинуть в шаблон
 
 description = 'В наборе данных хранить информации о продуктах JIO'
 
 task1 = 'Минимальная, максимальная, средняя цена в разрезе категорий'
-task2 = 'Минимальная, максимальная, средняя цена в разрезе категорий'
+task2 = 'Минимальная, максимальная, средняя цена в разрезе подкатегорий'
 task3 = 'Минимальная, максимальная, средняя цена за килограмм'
 
 @app.route('/')
 def home():
-    return max.to_html()
+    return sub_category.to_html()
 
 if __name__ == '__main__':
     app.run(debug=True, threaded=True)
