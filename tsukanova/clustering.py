193cf3f0317389c7318742020203d9216b9811ea
Inohara <InoSenju@Senju.com>
2023-12-03 23:24:47 +0400

    что

--- tsukanova/clustering.py
+++ tsukanova/clustering.py
@@ -1,5 +1,4 @@
 import random
-from array import array
 import numpy as np
 
 
@@ -16,17 +15,32 @@ class Clustering:
 
         print(self.centroids)
         clusters = [[] for i in range(self.k)]
+        prev_centroids = None
+        while np.not_equal(self.centroids, prev_centroids).any():
+            for item in data:
+                # расстояния от точки до каждого центроида
+                d = np.sqrt(np.sum((item - self.centroids)**2, axis=1))
+                # заносим точку в нужный кластер
+                clusters[np.argmin(d)].append(item)
+
+            prev_centroids = self.centroids
+            # новый центроид кластера - среднее значение принадлежащих ему точек
+            self.centroids = [np.mean(cluster, axis=0) for cluster in clusters]
+
+            # если у кластера нет точек, ему присваивается предыдущий центроид
+            for i, centroid in enumerate(self.centroids):
+                if np.isnan(centroid).any():
+                    self.centroids[i] = prev_centroids[i]
+
+    def adds(self, data):
+        centroids = []
+        centroid_id = []
+
         for item in data:
-            # расстояния от точки до каждого центроида
-            d = np.sqrt(np.sum((item - self.centroids)**2, axis=1))
-            # заносим точку в нужный кластер
-            clusters[np.argmin(d)].append(item)
+            d = np.sqrt(np.sum((item - self.centroids) ** 2, axis=1))
+            centroid_id = np.argmin(d)
+            centroids.append(self.centroids[centroid_id])
+            centroid_id.append(centroid_id)
 
-        prev_centroids = self.centroids
-        self.centroids = [np.mean(cluster, axis=0) for cluster in clusters]
-
-        # если у кластера нет точек, ему присваивается предыдущий центроид
-        for i, centroid in enumerate(self.centroids):
-            if np.isnan(centroid).any():
-                self.centroids[i] = prev_centroids[i]
+        return centroids, centroid_id
 
--- tsukanova/lab7.py
+++ tsukanova/lab7.py
@@ -13,14 +13,68 @@ app = Flask('__name__')
 
 df = pd.read_csv('csv_files/csv_encoded.csv')
 df.dropna(inplace=True)
-kmeans = Clustering()
-print(df[['category_mean_price', 'price']].iloc[0:3].to_numpy())
-kmeans.do(df[['category_mean_price', 'price']].iloc[0:3].to_numpy())
+
+# kmeans = Clustering()
+# print(df[['category_mean_price', 'price']].iloc[0:3].to_numpy())
+# kmeans.do(df[['category_mean_price', 'price']].iloc[0:3].to_numpy())
 # print(df[['category_mean_price', 'price']].to_numpy())
 
 
+@app.route('/')
+def home():
+    return render_template("lab7_home.html")
+
+
+@app.route('/plot_clustering', methods=['GET'])
+def plot_clustering():
+    data = request.args
+    _from = int(data['_from'])
+    _to = int(data['_to'])
+    if _from > _to:
+        _from, _to = _to, _from
+
+    cluster_count = int(data['k'])
+    encoded1 = before(_from, _to)
+    encoded2 = clustering(_from, _to, cluster_count)
+    return render_template("lab7_clustering.html",
+                           before=encoded1, after=encoded2)
+
+
+def clustering(_from, _to, k):
+    data = df[['category_mean_price', 'price']].iloc[0:3].to_numpy()
+
+    kmeans = Clustering(k)
+    kmeans.do(data)
+
+    centroids, centroid_id = kmeans.adds(data)
+    sns.scatterplot(x=[i[0] for i in data],
+                    y=[i[1] for i in data],
+                    hue=centroid_id,
+                    style=centroid_id,
+                    palette="deep",
+                    legend=None
+                    )
+
+    plt.plot([x for x, i in kmeans.centroids],
+             [y for i, y in kmeans.centroids],
+             markersize=10)
+
+    plt.xlabel("Средняя цена по категории")
+    plt.ylabel("Цена товара")
+
+    tmpfile = BytesIO()  # создание временного файла
+    plt.title("После кластеризации")
+    plt.savefig(tmpfile, format='png')
+    plt.clf()
+    plt.switch_backend('agg')
+    pic = base64.b64encode(tmpfile.getvalue()).decode('utf-8')  # кодирование
+    tmpfile.close()
+
+    return pic
+
+
 def before(_from, _to):
-    plt.xlabel("средняя цена по категории")
+    plt.xlabel("Средняя цена по категории")
     plt.ylabel("Цена товара")
     plt.scatter(df['category_mean_price'].iloc[_from:_to], df['price'].iloc[_from:_to], color='red')
 
@@ -33,3 +87,7 @@ def before(_from, _to):
     tmpfile.close()
 
     return pic
+
+
+if __name__ == '__main__':
+    app.run(debug=True, threaded=True, port=30000)
